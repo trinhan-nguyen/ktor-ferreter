88b27dea65c45301a6c025d5aadf4af95a933c50
Tri Nhan Nguyen <trinhan.nguyen.learner@gmail.com>
2023-12-16 18:11:49 -0500

    Print error with Status Pages

--- src/main/kotlin/com/ferreter/plugins/Routing.kt
+++ src/main/kotlin/com/ferreter/plugins/Routing.kt
@@ -1,18 +1,26 @@
 package com.ferreter.plugins
 
+import com.ferreter.Response
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, error ->
+            call.respondText(
+                text = error.localizedMessage,
+                contentType = ContentType.Text.Plain,
+                status = HttpStatusCode.InternalServerError,
+            )
+        }
+    }
+
     routing {
         get("/") {
-            call.respondText("Hello World!")
-        }
-        // Static plugin. Try to access `/static/index.html`
-        static("/static") {
-            resources("static")
+            call.respond(Response(status = "OK"))
         }
     }
 }
