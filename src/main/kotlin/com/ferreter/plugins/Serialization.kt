8f4a0da64dbd878d3377a9cb6b87e7207dd8b37d
Tri Nhan Nguyen <trinhan.nguyen.learner@gmail.com>
2024-01-05 23:36:41 -0500

    Set pretty print to true for serialization

--- src/main/kotlin/com/ferreter/plugins/Serialization.kt
+++ src/main/kotlin/com/ferreter/plugins/Serialization.kt
@@ -3,9 +3,14 @@ package com.ferreter.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                prettyPrint = true
+            }
+        )
     }
 }
