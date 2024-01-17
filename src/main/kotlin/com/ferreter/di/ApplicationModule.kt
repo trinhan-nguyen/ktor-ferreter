4e62290cb70aca9d7824b335705259290094824d
Tri Nhan Nguyen <trinhan.nguyen.learner@gmail.com>
2024-01-16 22:18:21 -0500

    Provide AlphaVantage api through Koin

--- src/main/kotlin/com/ferreter/di/ApplicationModule.kt
+++ src/main/kotlin/com/ferreter/di/ApplicationModule.kt
@@ -1,5 +1,11 @@
 package com.ferreter.di
 
+import com.ferreter.client.api.AlphaVantageApi
+import com.ferreter.client.api.AlphaVantageApiImpl
 import org.koin.dsl.module
 
-val applicationModule = module {}
+val applicationModule = module {
+    single<AlphaVantageApi> {
+        AlphaVantageApiImpl()
+    }
+}
--- src/main/kotlin/com/ferreter/plugins/Routing.kt
+++ src/main/kotlin/com/ferreter/plugins/Routing.kt
@@ -1,18 +1,19 @@
 package com.ferreter.plugins
 
 import com.ferreter.Response
-import com.ferreter.client.api.AlphaVantageApiImpl
+import com.ferreter.client.api.AlphaVantageApi
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(
+    alphaVantageApi: AlphaVantageApi
+) {
     val apiKey = environment.config.propertyOrNull(KTOR_ENV)?.getString()
-    val api = AlphaVantageApiImpl()
     routing {
         get("/") {
             apiKey?.let {
-                val quote = api.getQuote(
+                val quote = alphaVantageApi.getQuote(
                     symbol = "IBM",
                     apiKey = apiKey,
                 )
